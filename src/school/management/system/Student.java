d517196d155946c42a20c54b019d45a9bedf288f
Sahil Mishra <sahilmishra0012@outlook.com>
2019-03-09 09:31:09 +0530

    new changes

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -1,5 +1,18 @@
 package school.management.system;
 
-public class Student {
-
+public class Student
+{
+	private int id;
+	private String name;
+	private int grade;
+	private int feetotal;
+	
+	public Student(int id,String name,int grade,int feetotal)
+	{
+		this.id=id;
+		this.name=name;
+		this.grade=grade;
+		this.feetotal=feetotal;
+		
+	}
 }
