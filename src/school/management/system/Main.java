ad8dc1adae0dbd26b46d7813c636241e3fe98504
Sahil Mishra <sahilmishra0012@outlook.com>
2019-03-09 10:21:21 +0530

    New Changes

---
+++ src/school/management/system/Main.java
@@ -0,0 +1,20 @@
+package school.management.system;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("1-> Teacher\n2->Student");
+		int ch=sc.nextInt();
+		switch(ch)
+		{
+			case 1:
+				System.out.println("Enter teacher's ID, Name and Salary");
+				
+		}
+		
+		
+		sc.close();
+	}
+}
