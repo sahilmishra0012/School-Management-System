ebc542f7489374785d2127247854691cb5f75a2a
Sahil Mishra <sahilmishra0012@outlook.com>
2019-03-09 11:16:27 +0530

    New Changes

--- src/school/management/system/Main.java
+++ src/school/management/system/Main.java
@@ -4,27 +4,32 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+		boolean x=true;
 		Scanner sc=new Scanner(System.in);
-		System.out.println("1-> Teacher\n2->Student");
-		int ch=sc.nextInt();
-		switch(ch)
+		while(x)
 		{
-			case 1:
-				System.out.println("Enter teacher's ID, Name and Salary");
-				int tid=sc.nextInt();
-				String tname=sc.nextLine();
-				int salary=sc.nextInt();
-				Teacher name=new Teacher;
-			break;
-			case 2:
-				System.out.println("Enter Student's ID, Name and Grade");
-				int sid=sc.nextInt();
-				String sname=sc.nextLine();
-				int grade=sc.nextInt();
-				Teacher name=new Teacher
+			System.out.println("1-> Teacher\n2->Student");
+			int ch=sc.nextInt();
+			List<Teacher> teachers=new ArrayList<>();
+			List<Student> students=new ArrayList<>();
+			switch(ch)
+			{
+				case 1:
+					System.out.println("Enter teacher's ID, Name and Salary");
+					int tid=sc.nextInt();
+					String tname=sc.nextLine();
+					int salary=sc.nextInt();
+					teachers.add(new Teacher(tid,tname,salary));
+				break;
+				case 2:
+					System.out.println("Enter Student's ID, Name and Grade");
+					int sid=sc.nextInt();
+					String sname=sc.nextLine();
+					int grade=sc.nextInt();
+					students.add(new Student(sid,sname,grade));
+			}
 		}
-		
-		
+		System.out.println("Students and Teachers entered. Create School");
 		sc.close();
 	}
 }
