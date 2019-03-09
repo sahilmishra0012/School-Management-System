8f57817b5f509a522ebc97a21a20cf9973a6d2c3
Sahil Mishra <sahilmishra0012@outlook.com>
2019-03-09 10:03:27 +0530

    New Changes

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -7,4 +7,42 @@ public class School
 	private List<Student> students;
 	private int totalMoneyEarned;
 	private int totalMoneySpent;
+	
+	public School(List<Teacher> teachers, List<Student> students)
+	{
+		this.teachers = teachers;
+		this.students = students;
+	}
+	
+	public List<Teacher> getTeachers() {
+		return teachers;
+	}
+	
+	public void setTeachers(List<Teacher> teachers) {
+		this.teachers = teachers;
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+	
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
+	public int getTotalMoneyEarned() {
+		return totalMoneyEarned;
+	}
+	
+	public void setTotalMoneyEarned(int totalMoneyEarned) {
+		this.totalMoneyEarned = totalMoneyEarned;
+	}
+	
+	public int getTotalMoneySpent() {
+		return totalMoneySpent;
+	}
+	
+	public void setTotalMoneySpent(int totalMoneySpent) {
+		this.totalMoneySpent = totalMoneySpent;
+	}
 }
